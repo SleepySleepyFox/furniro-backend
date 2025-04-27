ddd0bf87302616e3a44a82cc2d1b7c8c1f4428d6
@Yevhen.Petrikeiev <ShkrebNsh@gmail.com>
2025-04-27 17:16:45 +0300

    password hashing

--- src/controllers/RegisterController.ts
+++ src/controllers/RegisterController.ts
@@ -2,13 +2,19 @@ import { RegisterService } from "../services/RegisterService"
 import { UserData } from "../types/UserData"
 import { Request, Response } from "express"
 import validateEmail from "../utils/EmailValidation"
+import hashPassword from "../utils/PasswordHashing"
 
 export const register = async (req: Request<{},{}, UserData>, res: Response) => {
     if(!validateEmail(req.body.Email)) {
         res.status(400).json({message: 'Invalid Email'})
     }else{
-        await RegisterService(req.body)
-        res.status(200)
+        const passwordHash = await hashPassword(req.body.Password)
+        const data: UserData = {
+            FirstName: req.body.FirstName,
+            SecondName: req.body.SecondName,
+            Email: req.body.Email,
+            Password: passwordHash
+        }
+        await RegisterService(data)
     }
-    // await console.log(req.body)
 }
